5a37a077b4e7526c3dd196d382b6cc00c3d781ed
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-11-22 18:49:25 -0300

    update adding trycatch

--- PluginAccountPreOperation.cs
+++ PluginAccountPreOperation.cs
@@ -12,53 +12,57 @@ namespace DynamicsPlugin
     {
         public void Execute(IServiceProvider serviceProvidor)
         {
-            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvidor.GetService(typeof(IPluginExecutionContext));
-
-            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvidor.GetService(typeof(IOrganizationServiceFactory));
-
-            IOrganizationService serviceAdmin = serviceFactory.CreateOrganizationService(null);
-
-            ITracingService trace = (ITracingService)serviceProvidor.GetService(typeof(ITracingService));
-
-            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            try
             {
-                Entity entidadeContext = (Entity)context.InputParameters["Target"];
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvidor.GetService(typeof(IPluginExecutionContext));
 
-                if (entidadeContext.LogicalName == "account")
+                IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvidor.GetService(typeof(IOrganizationServiceFactory));
+
+                IOrganizationService serviceAdmin = serviceFactory.CreateOrganizationService(null);
+
+                ITracingService trace = (ITracingService)serviceProvidor.GetService(typeof(ITracingService));
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
                 {
-                    if (entidadeContext.Attributes.Contains("telephone1"))
+                    Entity entidadeContext = (Entity)context.InputParameters["Target"];
+
+                    if (entidadeContext.LogicalName == "account")
                     {
-                        var phone1 = entidadeContext["telephone1"];
-                        string FetchContact = @"<?xml version='1.0'?>" +
-                        "<fetch distinct='false' mapping='logical' output-format='xml-platform' version='1.0'>" +
-                        "<entity name='contact'>" +
-                        "<attribute name='fullname'>" +
-                        "<attribute name='telephone1'>" +
-                        "<attribute name='contactid'>" +
-                        "<order descending='false' attribute='fullname' />" +
-                        "<filter type='and'>" +
-                        "<condition attribute='telephone1' value='" + phone1 + "' operator='eq'/>" +
-                        "</filter>" +
-                        "</entity>" +
-                        "</fetch>";
-
-                        trace.Trace("FetchContact: " + FetchContact);
-
-                        var primarycontact = serviceAdmin.RetrieveMultiple(new FetchExpression(FetchContact));
-
-                        if (primarycontact.Entities.Count > 0)
+                        if (entidadeContext.Attributes.Contains("telephone1"))
                         {
-                            foreach (var entityContact in primarycontact.Entities)
+                            var phone1 = entidadeContext["telephone1"];
+                            string FetchContact = @"<?xml version='1.0'?>" +
+                            "<fetch distinct='false' mapping='logical' output-format='xml-platform' version='1.0'>" +
+                            "<entity name='contact'>" +
+                            "<attribute name='fullname'>" +
+                            "<attribute name='telephone1'>" +
+                            "<attribute name='contactid'>" +
+                            "<order descending='false' attribute='fullname' />" +
+                            "<filter type='and'>" +
+                            "<condition attribute='telephone1' value='" + phone1 + "' operator='eq'/>" +
+                            "</filter>" +
+                            "</entity>" +
+                            "</fetch>";
+
+                            trace.Trace("FetchContact: " + FetchContact);
+
+                            var primarycontact = serviceAdmin.RetrieveMultiple(new FetchExpression(FetchContact));
+
+                            if (primarycontact.Entities.Count > 0)
                             {
-                                entidadeContext["primarycontactid"] = new EntityReference("contact", entityContact.Id);
+                                foreach (var entityContact in primarycontact.Entities)
+                                {
+                                    entidadeContext["primarycontactid"] = new EntityReference("contact", entityContact.Id);
+                                }
                             }
                         }
                     }
                 }
             }
-// catch(InvalidPluginExecutionException ex){
-//                 throw new InvalidPluginExecutionException("Error ocorrido: " + ex.Message);
-//             }
+            catch (InvalidPluginExecutionException ex)
+            {
+                throw new InvalidPluginExecutionException("Error ocorrido: " + ex.Message);
+            }
         }
     }
 }

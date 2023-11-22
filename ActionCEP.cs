9ce7a77f851a64e99207016ec101b969f78d28f7
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2023-11-22 20:16:13 -0300

    crated Action consuming an API

---
+++ ActionCEP.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Xrm.Sdk;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace DynamicsPlugin
+{
+    public class ActionCEP : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            IOrganizationService serviceAdmin = serviceFactory.CreateOrganizationService(null);
+
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            var cep = context.InputParameters["CepInput"];
+            trace.Trace("Cep informado: " + cep);
+
+            var viaCEPUrl = $"https://viacep.com.br/ws/{cep}/json/";
+
+            string result = string.Empty;
+
+            using (WebClient client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                client.Encoding = Encoding.UTF8;
+
+                result = client.DownloadString(viaCEPUrl);
+            }
+            context.OutputParameters["ResultadoCEP"] = result;
+            trace.Trace("Resultado: " + result);
+        }
+    }
+}
